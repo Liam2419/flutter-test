24280202cd36a42422974a0882ab36dd3620ed03
wei <forever.time75@gmail.com>
2023-11-06 16:53:58 -0800

    更新頁面 測試製作範例APP

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
 import 'dart:developer';
@@ -16,10 +18,87 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page1'),
+      home: const Covid19App(),
+      //home: const MyHomePage(title: 'Flutter Demo Home Page1'),
     );
   }
 }
+
+class Covid19App extends StatefulWidget {
+  const Covid19App({Key? key}) : super(key: key);
+
+  @override
+  State<Covid19App> createState() => _Covid19AppState();
+}
+
+class _Covid19AppState extends State<Covid19App> {
+  final int bgColor = 0xff1a1a;
+  final int textGrayColor = 0xffafafaf;
+  final int textRedColor = 0xffd40007;
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+        backgroundColor: Color(bgColor),
+        body: SafeArea(
+          child:Column(
+          children:[
+            TextColorType(name: 'data', color: textGrayColor, textSize: 40.0,),
+            TextColorType(name: '0', color: textRedColor, textSize: 35.0,),
+            Row(
+              children: [
+                SizedBox(
+                  width: size.width/2,
+                  child: Column(
+                    children:[
+                      TextColorType(name: 'data', color: textGrayColor, textSize: 40.0,),
+                      TextColorType(name: '0', color: textRedColor, textSize: 35.0,),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  width: size.width/2,
+                  child: Column(
+                    children:[
+                      TextColorType(name: 'data', color: textGrayColor , textSize: 40.0,),
+                      TextColorType(name: '0', color: textRedColor, textSize: 35.0,),
+                    ],
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+        ),
+      );
+  }
+}
+class TextColorType extends StatelessWidget {
+  const TextColorType({Key? key,required this.name,required this.color,required this.textSize}) : super(key: key);
+
+  final String name;
+  final int color;
+  final double textSize;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(name,
+      style: TextStyle(
+          color:Color(color),
+          fontSize: textSize,
+          fontWeight: FontWeight.w900,
+          shadows: const [Shadow(
+            color:  Color(0xff9ff013),
+            offset: Offset(1.0,1.0 ),
+          )]
+      ),
+    );
+  }
+}
+
+
+
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
@@ -44,7 +123,10 @@ class _MyHomePageState extends State<MyHomePage> {
   getDate() async{
     Response res;
     try {
-      res = await dio.get('https://reqres.in/api/users');
+      res = await dio.get('https://tdx.transportdata.tw/api/basic/v2/Bus/RealTimeByFrequency/Streaming/City/Hsinchu?%24top=30&%24format=JSON');
+      ///https://tech.iapps360.dev/datas/bus_time
+      ///https://reqres.in/api/users
+
       log("Index number is: ${res.statusCode}");
     }catch(e){
       log("x: $e");
